0557bc1834ced5039d097907fe84fd6cd4b91daf
eastminnn <oh1334000@gmail.com>
2025-09-11 15:03:22 +0900

    chore : 초기 환경세팅

---
+++ src/main/java/com/InFrame/InFrameWasApplication.java
@@ -0,0 +1,13 @@
+package com.InFrame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InFrameWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InFrameWasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InFrame/InFrameWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InFrame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InFrameWasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
