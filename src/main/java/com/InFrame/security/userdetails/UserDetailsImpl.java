48620f57323868be4e791b36ae012c21fc0cad79
eastminnn <oh1334000@gmail.com>
2025-09-25 15:44:42 +0900

    feat : UserDetails 추가

---
+++ src/main/java/com/InFrame/security/userdetails/UserDetailsImpl.java
@@ -0,0 +1,33 @@
+package com.InFrame.security.userdetails;
+
+import com.InFrame.domains.user.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String authority = user.getRole().name();
+        return Collections.singletonList(new SimpleGrantedAuthority(authority));
+    }
+}
