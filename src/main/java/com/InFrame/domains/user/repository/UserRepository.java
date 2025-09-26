46fba41b3fe89943cd046bc9b2e05f8dcd5a5f07
eastminnn <oh1334000@gmail.com>
2025-09-26 11:13:22 +0900

    feat : 레포지토리 추가

---
+++ src/main/java/com/InFrame/domains/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.InFrame.domains.user.repository;
+
+import com.InFrame.domains.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
