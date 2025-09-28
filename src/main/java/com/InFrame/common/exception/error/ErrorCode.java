45a8815d9b3fba0940d51982273543b1af4f03ee
eastminnn <oh1334000@gmail.com>
2025-09-28 18:10:04 +0900

    feat : 회원가입, 로그인 관련 에러 코드 추가

--- src/main/java/com/InFrame/common/exception/error/ErrorCode.java
+++ src/main/java/com/InFrame/common/exception/error/ErrorCode.java
@@ -11,7 +11,12 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "내부 서버 오류입니다."),
 
     // 유저 관련 오류
-    USER_NOT_FOUND(404, "해당 유저를 찾을 수 없습니다.");
+    USER_NOT_FOUND(404, "해당 유저를 찾을 수 없습니다."),
+
+    // 회원가입, 로그인 오류
+    EMAIL_ALREADY_EXIST(409, "해당 이메일은 이미 존재합니다."),
+    NICKNAME_ALREADY_EXIST(409, "해당 닉네임은 이미 존재합니다."),
+    INCORRECT_PASSWORD(400, "비밀번호가 일치하지 않습니다.");
 
     private final int status;
     private final String message;
