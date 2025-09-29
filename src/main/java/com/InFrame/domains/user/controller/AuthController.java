5bb2e203a082b6cb5c7c13c0c549dd6a6d084d6a
eastminnn <oh1334000@gmail.com>
2025-09-29 16:14:51 +0900

    feat : OAuth2 컨트롤러 추가

--- src/main/java/com/InFrame/domains/user/controller/AuthController.java
+++ src/main/java/com/InFrame/domains/user/controller/AuthController.java
@@ -4,13 +4,18 @@ import com.InFrame.domains.user.reqdto.SignInRequestDto;
 import com.InFrame.domains.user.reqdto.SignUpRequestDto;
 import com.InFrame.domains.user.resdto.AuthResponseDto;
 import com.InFrame.domains.user.service.AuthService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,5 +35,11 @@ public class AuthController {
         AuthResponseDto authResponseDto = authService.signin(signInRequestDto);
 
         return ResponseEntity.ok(authResponseDto);
+    }
+
+    @GetMapping("/oauth2/{provider}")
+    public void socialLogin(@PathVariable String provider, HttpServletResponse response) throws IOException {
+        String redirectUrl = "/oauth2/authorization/" + provider;
+        response.sendRedirect(redirectUrl);
     }
 }
