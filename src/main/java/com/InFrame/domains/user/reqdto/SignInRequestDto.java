99f1849a83b1c6e3a8e3ff9677a2d115c29fb553
eastminnn <oh1334000@gmail.com>
2025-10-04 12:48:41 +0900

    docs : Swagger 작성

--- src/main/java/com/InFrame/domains/user/reqdto/SignInRequestDto.java
+++ src/main/java/com/InFrame/domains/user/reqdto/SignInRequestDto.java
@@ -1,7 +1,18 @@
 package com.InFrame.domains.user.reqdto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Schema(description = "로그인 요청 DTO")
 public record SignInRequestDto(
+        @Schema(description = "사용자 이메일", example = "abcd1234@gmail.com")
+        @Email(message = "이메일 형식을 맞춰주세요.")
+        @NotBlank(message = "이메일은 필수 입력입니다.")
         String email,
+
+        @Schema(description = "비밀번호", example = "abcd1234")
+        @NotBlank(message = "비밀번호는 필수 입력입니다.")
         String password
 ) {
 }
