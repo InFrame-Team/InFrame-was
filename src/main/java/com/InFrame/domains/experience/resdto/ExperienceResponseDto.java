62fb7a76f964b9b11c6c1b1325767a63de783d9c
eastminnn <oh1334000@gmail.com>
2025-10-29 03:49:45 +0900

    feat : 체험 응답 필드 추가

--- src/main/java/com/InFrame/domains/experience/resdto/ExperienceResponseDto.java
+++ src/main/java/com/InFrame/domains/experience/resdto/ExperienceResponseDto.java
@@ -2,14 +2,24 @@ package com.InFrame.domains.experience.resdto;
 
 import com.InFrame.domains.experience.entity.Experience;
 import com.InFrame.domains.host.entity.Host;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.Set;
+
+@Schema(description = "체험 정보 응답 DTO")
 public record ExperienceResponseDto (
         Long experienceId,
         Long hostId,
         String title,
         String description,
         int price,
-        int time
+        int durationInHours,
+        int maxCapacityPerSlot,
+        Set<DayOfWeek> availableDaysOfWeek,
+        Set<LocalTime> availableTimes
+
 ) {
     public static  ExperienceResponseDto from(Experience experience, Host host) {
         return  new ExperienceResponseDto(
@@ -18,7 +28,10 @@ public record ExperienceResponseDto (
                 experience.getTitle(),
                 experience.getDescription(),
                 experience.getPrice(),
-                experience.getTime()
+                experience.getDurationInHours(),
+                experience.getMaxCapacityPerSlot(),
+                experience.getAvailableDaysOfWeek(),
+                experience.getAvailableTimes()
         );
     }
 }
