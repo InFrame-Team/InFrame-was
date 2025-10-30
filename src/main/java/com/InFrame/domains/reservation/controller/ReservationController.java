c368079fbf3978cfdecb3c5092836db542029b47
eastminnn <oh1334000@gmail.com>
2025-10-30 19:00:07 +0900

    feat : Reservation 컨트롤러 추가

---
+++ src/main/java/com/InFrame/domains/reservation/controller/ReservationController.java
@@ -0,0 +1,49 @@
+package com.InFrame.domains.reservation.controller;
+
+import com.InFrame.domains.reservation.reqdto.ReservationRequestDto;
+import com.InFrame.domains.reservation.resdto.AvailableSlotDto;
+import com.InFrame.domains.reservation.resdto.ReservationResponseDto;
+import com.InFrame.domains.reservation.service.ReservationService;
+import com.InFrame.security.userdetails.UserDetailsImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @GetMapping("/experiences/{experienceId}/available-slots")
+    public ResponseEntity<?> getAvailableSlots(
+            @PathVariable Long experienceId,
+            @RequestParam LocalDate date) {
+
+        List<AvailableSlotDto> slots = reservationService.getAvailableSlots(experienceId, date);
+
+        return ResponseEntity.ok(slots);
+    }
+
+    @PostMapping("/reservation")
+    public ResponseEntity<?> createReservation(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody @Valid ReservationRequestDto requestDto) {
+
+        ReservationResponseDto responseDto = reservationService.createReservation(userDetails.getUser(), requestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+}
