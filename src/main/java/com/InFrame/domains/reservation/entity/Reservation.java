b5c1f3786e6224b2cba298d277c2564b5790d062
eastminnn <oh1334000@gmail.com>
2025-10-30 18:55:00 +0900

    feat : Reservation 엔티티 추가

---
+++ src/main/java/com/InFrame/domains/reservation/entity/Reservation.java
@@ -0,0 +1,62 @@
+package com.InFrame.domains.reservation.entity;
+
+import com.InFrame.domains.experience.entity.Experience;
+import com.InFrame.domains.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime reservedStartTime; // 예약된 시작 시간
+
+    @Column(nullable = false)
+    private int numAdults; // 예약한 성인 수
+
+    @Column(nullable = false)
+    private int numChildren; // 예약한 아동 수
+
+    @Schema(description = "총 예약 인원")
+    int totalParticipants;
+
+    @Column(nullable = false)
+    private int totalPrice; // 최종 결제(예약) 금액
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt; // 예약 생성 시간
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Experience experience;
+
+    @Builder
+    public Reservation(LocalDateTime reservedStartTime, int numChildren, int numAdults,
+                       int totalPrice, LocalDateTime createdAt, User user, Experience experience) {
+        this.reservedStartTime = reservedStartTime;
+        this.numAdults = numAdults;
+        this.numChildren = numChildren;
+        this.totalPrice = totalPrice;
+        this.createdAt = LocalDateTime.now();
+        this.user = user;
+        this.experience = experience;
+    }
+}
