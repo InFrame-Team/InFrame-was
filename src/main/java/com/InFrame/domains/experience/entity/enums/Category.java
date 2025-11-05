c17b0e113670c71f6cc26e19b05c7ab6d7d68d0c
eastminnn <oh1334000@gmail.com>
2025-11-05 16:46:20 +0900

    feat : 분야 카테고리, 전문 분야, 상세 분야 enum 추가

---
+++ src/main/java/com/InFrame/domains/experience/entity/enums/Category.java
@@ -0,0 +1,9 @@
+package com.InFrame.domains.experience.entity.enums;
+
+// 분야 카테고리
+public enum Category {
+    MASTER_ARTISAN, // 장인, 명인
+    YOUNG_ENTREPRENEUR, // 청년사업가
+    LOCAL_MERCHANT, // 골목상인
+    ARTIST // 예술가, 문화인
+}
---
+++ src/main/java/com/InFrame/domains/experience/entity/enums/ProfessionalField.java
@@ -0,0 +1,13 @@
+package com.InFrame.domains.experience.entity.enums;
+
+// 전문분야
+public enum ProfessionalField {
+    CRAFT_CREATION, // 공예, 창작
+    FOOD_DESSERT, // 음식, 디저트
+    FLOWER_GARDENING, // 플라워, 가드닝
+    CULTURE_TRADITION, // 문화, 전통체험
+    MUSIC_ART, // 음악/예술
+    LIFE_HEALING, // 라이프, 힐링
+    LOCAL_TOUR, // 지역탐방/체험투어
+    PHOTO_CONTENT // 사진, 콘텐츠
+}
