83bc04798651a21d4814a8aca3f4879645ddad01
eastminnn <oh1334000@gmail.com>
2025-11-05 17:04:28 +0900

    feat : 분야 카테고리, 전문 분야, 상세 분야 enum 추가

--- src/main/java/com/InFrame/domains/experience/entity/enums/DetailField.java
+++ src/main/java/com/InFrame/domains/experience/entity/enums/DetailField.java
@@ -1,13 +1,20 @@
 package com.InFrame.domains.experience.entity.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 // 상세분야
+@Getter
+@RequiredArgsConstructor
 public enum DetailField {
-    CERAMIC_CRAFT, // 도자기 공예
-    LEATHER_CRAFT, // 가죽 공예
-    METAL_SILVER_CRAFT,  // 금속, 은공예
-    SOAP_CANDLE_DIFFUSER, // 비누, 캔들, 디퓨저 제작
-    WOOD_CRAFT, // 목공예
-    DRAWING_WATERCOLOR, // 드로잉, 수채화
-    PERFUME_MAKING, // 향수 만들기
-    KNITTING_EMBROIDERY // 뜨개, 자수 클래스
+    CERAMIC_CRAFT("도자기 공예"),
+    LEATHER_CRAFT("가죽 공예"),
+    METAL_SILVER_CRAFT("금속/은공예"),
+    SOAP_CANDLE_DIFFUSER("비누/캔들/디퓨저 제작"),
+    WOOD_CRAFT("목공예"),
+    DRAWING_WATERCOLOR("드로잉/수채화"),
+    PERFUME_MAKING("향수 만들기"),
+    KNITTING_EMBROIDERY("뜨개/자수 클래스");
+
+    private final String description;
 }
--- src/main/java/com/InFrame/domains/experience/entity/enums/ProfessionalField.java
+++ src/main/java/com/InFrame/domains/experience/entity/enums/ProfessionalField.java
@@ -1,13 +1,20 @@
 package com.InFrame.domains.experience.entity.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 // 전문분야
+@Getter
+@RequiredArgsConstructor
 public enum ProfessionalField {
-    CRAFT_CREATION, // 공예, 창작
-    FOOD_DESSERT, // 음식, 디저트
-    FLOWER_GARDENING, // 플라워, 가드닝
-    CULTURE_TRADITION, // 문화, 전통체험
-    MUSIC_ART, // 음악/예술
-    LIFE_HEALING, // 라이프, 힐링
-    LOCAL_TOUR, // 지역탐방/체험투어
-    PHOTO_CONTENT // 사진, 콘텐츠
+    CRAFT_CREATION("공예/창작"),
+    FOOD_DESSERT("음식/디저트"),
+    FLOWER_GARDENING("플라워/가드닝"),
+    CULTURE_TRADITION("문화/전통체험"),
+    MUSIC_ART("음악/예술"),
+    LIFE_HEALING("라이프/힐링"),
+    LOCAL_TOUR("지역탐방/체험투어"),
+    PHOTO_CONTENT("사진/콘텐츠");
+
+    private final String description;
 }
