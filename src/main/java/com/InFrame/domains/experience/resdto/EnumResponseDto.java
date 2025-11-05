519d3ea4a23e83f1917caa7ec0b4eed2cdbe3f1e
eastminnn <oh1334000@gmail.com>
2025-11-05 17:21:41 +0900

    refactor : record 로 변경

--- src/main/java/com/InFrame/domains/experience/resdto/EnumResponseDto.java
+++ src/main/java/com/InFrame/domains/experience/resdto/EnumResponseDto.java
@@ -1,14 +1,4 @@
 package com.InFrame.domains.experience.resdto;
 
-import lombok.Getter;
-
-@Getter
-public class EnumResponseDto {
-    private final String code;
-    private final String description;
-
-    public EnumResponseDto(String code, String description) {
-        this.code = code;
-        this.description = description;
-    }
+public record EnumResponseDto(String code, String description) {
 }
