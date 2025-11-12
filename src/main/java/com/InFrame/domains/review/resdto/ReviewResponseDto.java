62115f1eb218f995a813e2038873abe919bf71e1
eastminnn <oh1334000@gmail.com>
2025-11-12 14:11:49 +0900

    feat : 리뷰 응답 dto 추가

---
+++ src/main/java/com/InFrame/domains/review/resdto/ReviewResponseDto.java
@@ -0,0 +1,31 @@
+package com.InFrame.domains.review.resdto;
+
+import com.InFrame.domains.review.entity.Review;
+
+import java.time.LocalDateTime;
+
+public record ReviewResponseDto(
+        Long reviewId,
+        String writerNickname,
+        String hostName,
+        Long experienceId,
+        String experienceTitle,
+        int rating,
+        String comment,
+        String reviewImageUrl,
+        LocalDateTime createdAt
+) {
+    public static ReviewResponseDto from(Review review) {
+        return new ReviewResponseDto(
+                review.getId(),
+                review.getUser().getNickname(),
+                review.getReservation().getExperience().getHost().getUser().getName(),
+                review.getReservation().getExperience().getId(),
+                review.getReservation().getExperience().getTitle(),
+                review.getRating(),
+                review.getComment(),
+                review.getReviewImageUrl(),
+                review.getCreatedAt()
+        );
+    }
+}
