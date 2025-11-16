ed6d804a799b8a27c14570231ab55bdce2159839
eastminnn <oh1334000@gmail.com>
2025-11-17 02:49:33 +0900

    feat : 호스트 하트 엔티티 추가

---
+++ src/main/java/com/InFrame/domains/like/entity/HostLike.java
@@ -0,0 +1,37 @@
+package com.InFrame.domains.like.entity;
+
+import com.InFrame.domains.host.entity.Host;
+import com.InFrame.domains.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class HostLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "host_id", nullable = false)
+    private Host host;
+
+    @Builder
+    public HostLike(User user, Host host) {
+        this.user = user;
+        this.host = host;
+    }
+}
