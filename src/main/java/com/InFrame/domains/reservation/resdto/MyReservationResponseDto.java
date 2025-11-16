f0857dd7875d5739cb37187b2ef76fe737a51ac4
eastminnn <oh1334000@gmail.com>
2025-11-17 04:43:30 +0900

    feat : 내 예약내역 조회 응답 DTO 수정

--- src/main/java/com/InFrame/domains/reservation/resdto/MyReservationResponseDto.java
+++ src/main/java/com/InFrame/domains/reservation/resdto/MyReservationResponseDto.java
@@ -1,7 +1,10 @@
 package com.InFrame.domains.reservation.resdto;
 
+import com.InFrame.domains.experience.entity.Experience;
+import com.InFrame.domains.host.entity.Host;
 import com.InFrame.domains.reservation.entity.Reservation;
 import com.InFrame.domains.reservation.entity.enums.ReservationStatus;
+import com.InFrame.domains.user.entity.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
@@ -20,6 +23,9 @@ public record MyReservationResponseDto(
         @Schema(description = "호스트 이름")
         String hostName,
 
+        @Schema(description = "호스트 프로필 이미지")
+        String hostProfileImageUrl,
+
         @Schema(description = "체험 제목")
         String experienceTitle,
 
@@ -29,18 +35,32 @@ public record MyReservationResponseDto(
         @Schema(description = "총 가격")
         int totalPrice,
 
+        @Schema(description = "예약 생성 시간")
+        LocalDateTime createdAt,
+
         @Schema(description = "리뷰 작성 여부")
         boolean reviewWritten
 ) {
     public static MyReservationResponseDto from(Reservation reservation, boolean reviewWritten) {
+        Experience experience = reservation.getExperience();
+        Host host = experience.getHost();
+        User hostUser = host.getUser();
+
+        String imageUrl = null;
+        if (experience.getImageUrls() != null && !experience.getImageUrls().isEmpty()) {
+            imageUrl = experience.getImageUrls().stream().findFirst().orElse(null);
+        }
+
         return new MyReservationResponseDto(
                 reservation.getId(),
-                reservation.getExperience().getId(),
+                experience.getId(),
                 reservation.getStatus(),
-                reservation.getExperience().getHost().getUser().getName(),
-                reservation.getExperience().getTitle(),
+                hostUser.getName(),
+                hostUser.getProfileImageUrl(),
+                experience.getTitle(),
                 reservation.getReservedStartTime(),
                 reservation.getTotalPrice(),
+                reservation.getCreatedAt(),
                 reviewWritten
         );
     }
