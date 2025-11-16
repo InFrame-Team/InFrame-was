51493d4e625d41d49a529ac9ea234fb155a345c2
eastminnn <oh1334000@gmail.com>
2025-11-17 02:54:18 +0900

    feat : 좋아요를 위한 쿼리 추가

--- src/main/java/com/InFrame/domains/experience/repository/ExperienceRepository.java
+++ src/main/java/com/InFrame/domains/experience/repository/ExperienceRepository.java
@@ -4,6 +4,7 @@ import com.InFrame.domains.experience.entity.Experience;
 import com.InFrame.domains.host.entity.Host;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,4 +19,9 @@ public interface ExperienceRepository extends JpaRepository<Experience, Long> {
     // 체험을 가진 호스트 조회
     @Query("SELECT DISTINCT e.host FROM Experience e")
     List<Host> findDistinctHostsWithExperiences();
+
+    @Query("SELECT DISTINCT e FROM Experience e " +
+            "LEFT JOIN FETCH e.imageUrls " +
+            "WHERE e.host IN :hosts")
+    List<Experience> findAllByHostInWithImages(@Param("hosts") List<Host> hosts);
 }
